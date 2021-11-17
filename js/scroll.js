16f091304ec2bc3819f5ed3fac79c4ec458a2041
SANKALP1011 <tigerishan426@gmail.com>
2021-11-17 20:33:11 +0530

    Links added for the project.

--- js/scroll.js
+++ js/scroll.js
@@ -10,8 +10,5 @@
 //       .setClassToggle('.MidViewDesign','fade-in') // add the class
 //       .addTo(controller)
 // });
-AOS.init({
-  duration: 2000,
-})
 var CuurentDate = new Date();
-document.getElementById("date").innerHTML=CuurentDate;
+document.getElementsByClassName("dateText").innerHtml = CuurentDate
